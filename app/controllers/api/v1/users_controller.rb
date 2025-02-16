3e51839d2abbf67253776129697adcb920a49ef4
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-02-16 19:50:39 +0900

    fix: update controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,9 +20,21 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user.as_json(except: [:password_digest]), status: :ok # 200
+    else
+      render json: @user.errors, status: :unprocessable_entity # 422
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      render json: @user.as_json(except: [:password_digest]), status: :ok # 200
+    else
+      render json: @user.errors, status: :unprocessable_entity # 422
+    end
   end
 
   private
