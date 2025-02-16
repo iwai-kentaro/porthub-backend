0bd1fcfc482297773e796a8cdff5b2f376da7b70
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-02-16 22:54:00 +0900

    feat: login me routingを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :projects, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:index, :show, :create, :update, :destroy]
+
+      post "auth/login", to: "#auth/login"
+      get "auth/me", to: "#auth/me"
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
