b07411dd464ab9d204f933242dab6f20f02fc304
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-02-16 19:54:49 +0900

    feat: create projects controller

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,16 +1,44 @@
 class Api::V1::ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+    render json: @projects
   end
 
   def show
+    @project = Project.find(params[:id])
+    render json: @project
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      render json: @project.as_json, status: :created # 201
+    else
+      render json: @project.errors, status: :unprocessable_entity # 422
+    end
   end
 
   def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      render json: @project.as_json, status: :ok # 200
+    else
+      render json: @project.errors, status: :unprocessable_entity # 422
+    end
   end
 
   def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      render json: @project.as_json, status: :ok # 200
+    else
+      render json: @project.errors, status: :unprocessable_entity # 422
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :user_id, :tag, :image, :portfolio_url)
   end
 end
