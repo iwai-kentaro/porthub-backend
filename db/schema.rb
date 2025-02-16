0873f0ae8846629ea4ffee88be5918dae25e7654
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-02-16 21:21:44 +0900

    feat: emailの重複防止、migration実施

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_15_133606) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_16_121243) do
   create_table "projects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.string "tag"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_15_133606) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "projects", "users"
