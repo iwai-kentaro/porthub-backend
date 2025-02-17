33b2819d5c7c169eefe9f764959ecf3ac236ac71
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-02-17 23:46:56 +0900

    feat: gemの追加jwt routeの修正　jwtのdecodeの方法を変更

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,10 +1,12 @@
+require 'jwt' 
+
 class Api::V1::AuthController < ApplicationController
   before_action :authenticate_user, only: [:me]
 
   def login
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      @token = JWT.encode({ user_id: @user.id }, Rails.application.secrets.secret_key_base[0])
+      @token = JWT.encode({ user_id: @user.id }, Rails.application.secret_key_base, 'HS256')
       render json: {token: @token, user: {id: @user.id, username: @user.username, email: @user.email}}, status: :ok
     else
       render json: {error: "Invalid email or password"}, status: :unauthorized
@@ -24,7 +26,7 @@ class Api::V1::AuthController < ApplicationController
 
   def authenticate_user
     @token = request.headers["Authorization"]&.split(" ")&.last
-    @decoded = JWT.decoded(@token, Rails.application.secrets.secret_key_base[0]).first rescue nil
+    @decoded = JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256').first
     @current_user = User.find(@decoded["user_id"]) if @decoded
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
       resources :projects, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:index, :show, :create, :update, :destroy]
 
-      post "auth/login", to: "#auth/login"
-      get "auth/me", to: "#auth/me"
+      post "auth/login", to: "auth#login"
+      get "auth/me", to: "auth#me"
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
